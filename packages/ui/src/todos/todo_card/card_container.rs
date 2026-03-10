d2cf524db7f5eaf842ff88f0085a42a3d7d22a90
Bruno Berrehuel <bruno@homepad.ch>
2026-03-10 13:58:30 +0100

    feat: toggle todo/done status

--- packages/ui/src/todos/todo_card/card_container.rs
+++ packages/ui/src/todos/todo_card/card_container.rs
@@ -7,16 +7,18 @@ use crate::todos::{todo_card::ToggleTodo, RemoveTodo};
 pub fn CardContainer(todo: Todo) -> Element {
     let id = todo.id;
     let mut hover = use_signal(|| false);
+    let completed = use_signal(|| todo.completed);
 
     rsx! {
         div {
-            class:"relative flex justify-between bg-white mb-2 w-full rounded-lg p-2 border border-neutral-300 hover:border-neutral-500 hover:shadow-lg transition-all",
+            class:"relative flex items-center justify-between bg-white mb-2 w-full rounded-lg p-2 border border-neutral-300 hover:border-neutral-500 hover:shadow-lg transition-all",
             onmouseenter: move |_| hover.set(true),
             onmouseleave: move |_| hover.set(false),
             div {
+                class: if completed() { "line-through text-neutral-200"},
                 {todo.content.clone()}
             }
-            ToggleTodo { todo }
+            ToggleTodo { todo, completed }
 
             if hover() {
                 div {
--- packages/ui/src/todos/todo_card/toggle_todo.rs
+++ packages/ui/src/todos/todo_card/toggle_todo.rs
@@ -2,29 +2,22 @@ use api::{toggle_todo, Todo};
 use dioxus::prelude::*;
 
 #[component]
-pub fn ToggleTodo(todo: Todo) -> Element {
-    let mut completed = use_signal(|| todo.completed);
+pub fn ToggleTodo(todo: Todo, completed: Signal<bool>) -> Element {
+    // use optimistic update of a local value
     let toggle = move |_| async move {
         *completed.write() = !completed();
+        // get back if BE error
         if toggle_todo(todo.id).await.is_err() {
-            *completed.write() = completed();
+            *completed.write() = !completed();
         }
     };
 
     rsx! {
-        if completed() {
-            div {
-                class: "bg-green-200",
-                onclick: toggle,
-                "Done"
-            }
-        } else {
-            div {
-                class: "bg-red-200",
-                onclick: toggle,
-                "Todo"
-            }
-
+        div {
+            class: "text-center w-16  px-2 py-1 mr-6 cursor-pointer rounded-sm",
+            class: if completed() {"bg-green-200"} else {"bg-red-200"},
+            onclick: toggle,
+            if completed() {"Done"} else {"Todo"}
         }
     }
 }
